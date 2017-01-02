1bef6682c2d14d82673c41f2e55ff666f95de286
Elmer de Looff <elmer.delooff@gmail.com>
2017-01-03 00:11:05 +0100

    Added dependency link to setup.py and refactored entry points.

--- setup.py
+++ setup.py
@@ -4,15 +4,16 @@ from setuptools import setup
 
 setup(
     name='pretix-icepay',
-    version="0.0.1",
-    description="Pretix extension for the Icepay payment provider.",
+    version='0.0.1',
+    description='Pretix extension for the Icepay payment provider.',
     author='chotee',
     author_email='chotee@openended.eu',
+    dependency_links=[
+        'git+https://github.com/edelooff/icepay-python.git#egg=icepay-python',
+    ],
     packages=['pretix_icepay'],
     install_requires=['icepay_python'],
-    url="https://github.com/chotee/pretix-icepay",
-    entry_points="""
-[pretix.plugin]
-pretix_icepay=pretix_icepay:PretixPluginMeta
-"""
+    url='https://github.com/chotee/pretix-icepay',
+    entry_points={
+        'pretix.plugin': 'pretix_icepay = pretix_icepay:PretixPluginMeta'}
 )
